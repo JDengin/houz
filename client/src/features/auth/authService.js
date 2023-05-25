8c177dcd5a952781a4375bf954aa4c07e7f78c83
JDesso <jdessouma564@gmail.com>
2023-05-25 11:15:33 +0200

    few changes

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
-//const API_URL = '/user/signup/'
+//const API_URL = `http://localhost:8080/user`
 
-const API_URL = `http://localhost:8080/user`
-//const API_URL = 'https://houz-dhqm.onrender.com/user/';
+const API_URL = 'https://houz-koe3.onrender.com/user'; 
 
 // Register user
 const register = async (userData) => {
